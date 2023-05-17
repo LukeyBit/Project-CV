08c1730a4cf469f51c505189f551fe3615e92bc4
Lukas Andersson <lukas@lukasanderson.com>
2023-05-17 17:49:55 +0200

    Made navbar responsive client component

--- client/src/app/Navbar.tsx
+++ client/src/app/Navbar.tsx
@@ -1,3 +1,4 @@
+'use client';
 import { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars } from '@fortawesome/free-solid-svg-icons';
@@ -11,18 +12,14 @@ export default function Navbar() {
     }
 
     return (
-        <nav data-test-id='nav' className='flex items-center justify-between flex-wrap container-glass rounded-t-none p-6 md'>
-            <div className='flex items-center flex-shrink-0 text-white mr-6'>
-                <span className='font-semibold text-2xl tracking-tight'>
-                    <Link href='/' className='hover:text-gray-800'>Lukas Andersson</Link>
-                </span>
-            </div>
-            <div className='block md:hidden'>
-                <button className='flex items-center px-3 py-2 border-2 rounded text-white border-white hover:text-gray-800 hover:bg-white' onClick={toggleOpen}>
+        <nav data-test-id='nav' className='flex flex-col md:flex-row md:items-center justify-between container-glass rounded-t-none p-6 md'>
+            <div className='flex flex-row justify-between items-center text-white mr-6'>
+                <Link href='/' className='font-semibold text-2xl tracking-tight hover:text-gray-800'>Lukas Andersson</Link>
+                <button className='md:hidden flex items-center px-3 py-2 border-2 rounded text-white border-white hover:text-gray-800 hover:bg-white' onClick={toggleOpen}>
                     <FontAwesomeIcon icon={faBars} className='w-8 h-8' />
                 </button>
             </div>
-            <div className='md:flex items-center flex-shrink-0 text-white mr-6 gap-4 text-lg hidden'>
+            <div className={`md:flex md:items-center text-white mr-6 gap-4 text-lg ${open ? 'flex flex-col mt-4' : 'hidden'}`}>
                 <Link href='/Contact' className='text-white hover:text-gray-800 md:text-gray-800 md:hover:bg-gray-300 md:bg-white md:p-2 md:rounded'>Contact</Link>
                 <Link href='/Portfolio' className='text-white hover:text-gray-800 md:text-gray-800 md:hover:bg-gray-300 md:bg-white md:p-2 md:rounded'>Portfolio</Link>
                 <Link href='/Resume' className='text-white hover:text-gray-800 md:text-gray-800 md:hover:bg-gray-300 md:bg-white md:p-2 md:rounded'>Resume</Link>
