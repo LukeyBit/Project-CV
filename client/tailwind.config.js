06b6358528b1c47c2dd907e9eafe36c0875e5cc3
Lukas Andersson <lukas@lukasanderson.com>
2023-05-18 21:35:52 +0200

    Updated Tailwind config, Extracted form to component

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,5 +5,15 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx,mdx}',
     './src/app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
+  theme: {
+    extend: {
+      colors: {
+        success:'#00ff00',
+        error: '#ff0000',
+        warning: '#ffff00',
+        info: '#0000ff',
+      },
+    },
+  },
   plugins: [],
 }
