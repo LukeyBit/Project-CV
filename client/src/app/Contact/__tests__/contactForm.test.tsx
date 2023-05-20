b2d795e6355c76f50a6111e5f81ae60858aa8e76
Lukas Andersson <lukas@lukasanderson.com>
2023-05-21 00:04:00 +0200

    updated tsconfig in server

--- client/src/app/Contact/__tests__/contactForm.test.tsx
+++ client/src/app/Contact/__tests__/contactForm.test.tsx
@@ -1,1 +1,22 @@
 // TODO Unit test for contactForm
+
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import Contact from '@/app/Contact/page';
+
+jest.mock('axios');
+
+describe('Contact', () => {
+    it('renders a main element', () => {
+        render(<Contact />);
+        expect(screen.getByRole('main')).toBeInTheDocument();
+    });
+    it('renders a main element with a flexbox', () => {
+        render(<Contact />);
+        expect(screen.getByRole('main')).toHaveClass('flex');
+    });
+    it('renders a main element with a flexbox with a minimum height of 100vh', () => {
+        render(<Contact />);
+        expect(screen.getByRole('main')).toHaveClass('min-h-screen');
+    });
+});
