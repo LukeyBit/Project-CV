15f881203b0aa5002c1b2fcf28c8f5d61fd0b9b7
Lukas Andersson <lukas@lukasanderson.com>
2023-05-25 02:27:22 +0200

    FIxed jest in server

--- server/src/__tests__/index.test.ts
+++ server/src/__tests__/index.test.ts
@@ -1,6 +1,15 @@
 import app from '../index';
 import supertest from 'supertest';
 
+jest.mock('mongoose', () => {
+    return {
+        connect: jest.fn(() => Promise.resolve('Connected to MongoDB')),
+        Schema: jest.fn(),
+        model: jest.fn(),
+    };
+});
+
+
 describe('Test the root path', () => {
     test('It should respond 400 with a message to the empty POST method', async () => {
         const response = await supertest(app).post('/message');
--- server/src/routes/messageRouter.ts
+++ server/src/routes/messageRouter.ts
@@ -3,38 +3,32 @@ import { saveMessage } from '../controllers/dbController';
 import { sendMessage } from '../controllers/mailController';
 
 export interface ControllerResponse {
-  success: boolean;
-  message: string;
+    success: boolean;
+    message: string;
 }
 
 const router = express.Router();
 
 router.post('/', (req: Request, res: Response) => {
-  if (
-    !req.body.firstName ||
-    !req.body.lastName ||
-    !req.body.email ||
-    !req.body.message
-  ) {
-    res.status(400).json({ message: 'Missing required fields' });
-  }
-  const mailRes: ControllerResponse = sendMessage(req);
-  saveMessage(req)
-    .then((dbRes: ControllerResponse) => {
-      if (dbRes.success && mailRes.success) {
-        res.status(200).json({ message: 'Message sent and saved' });
-      } else if (dbRes.success && !mailRes.success) {
-        res.status(200).json({ message: 'Message saved' });
-      } else if (!dbRes.success && mailRes.success) {
-        res.status(200).json({ message: 'Message sent' });
-      } else if (!dbRes.success && !mailRes.success) {
-        res.status(500).json({ message: 'Message not sent or saved' });
-      }
-    })
-    .catch((err: Error) => {
-      console.error(err);
-      res.status(500).json({ message: 'Message not sent or saved' });
-    });
+    if (!req.body.firstName || !req.body.lastName || !req.body.email || !req.body.message) {
+        res.status(400).json({ message: 'Missing required fields' });
+    }
+    const mailRes: ControllerResponse = sendMessage(req);
+    saveMessage(req)
+        .then((dbRes: ControllerResponse) => {
+            if (dbRes.success && mailRes.success) {
+                res.status(200).json({ message: 'Message sent and saved' });
+            } else if (dbRes.success && !mailRes.success) {
+                res.status(200).json({ message: 'Message saved' });
+            } else if (!dbRes.success && mailRes.success) {
+                res.status(200).json({ message: 'Message sent' });
+            } else if (!dbRes.success && !mailRes.success) {
+                res.status(500).json({ message: 'Message not sent or saved' });
+            }
+        })
+        .catch((err: Error) => {
+            console.error(err);
+        });
 });
 
 export default router;
