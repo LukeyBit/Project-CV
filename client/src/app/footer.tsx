5d8c656958ca832b81742bbe6e5683a10c256439
Lukas Andersson <lukas@lukasanderson.com>
2023-06-07 22:24:51 +0200

    Added information and github link to Home

--- client/src/app/footer.tsx
+++ client/src/app/footer.tsx
@@ -6,7 +6,7 @@ export default function Footer() {
         <footer className='flex flex-col md:flex-row items-center justify-center w-full h-fit container-glass rounded-b-none gap-4 py-4'>
             <p>Copyright © 2023 Lukas Andersson</p>
             <a href='https://github.com/LukeyBit' className='hover:text-gray-800 flex flex-row gap-2 items-center'>
-                <FontAwesomeIcon icon={faGithub} className='w-8 h-8' /> @LukeyBit
+                <FontAwesomeIcon icon={faGithub} className='w-8 h-8' /> /LukeyBit
             </a>
         </footer>
     );
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,3 +1,6 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub } from '@fortawesome/free-brands-svg-icons';
+
 export default function Home() {
   return (
     <main className='flex min-h-screen flex-col items-center gap-20 md:gap-28 md:p-16 md:py-28 py-10'>
@@ -15,13 +18,22 @@ export default function Home() {
           <h1 className='text-4xl lg:text-6xl font-bold'>Website</h1>
         </div>
       </div>
-      <div className='container-glass flex flex-col items-center justify-center gap-6 w-full md:w-[90%] lg:w-[70%] px-6 py-10 lg:p-14'>
+      <div className='container-glass flex flex-col items-center justify-center gap-6 w-full md:w-[90%] lg:w-[70%] px-6 pt-10 pb-6 lg:pt-14 pb-8'>
         <div className='flex flex-col items-center justify-center'>
           <h1 className='block md:hidden text-center text-4xl lg:text-6xl font-bold'>Welcome to my website</h1>
-          <h2 className='text-2xl lg:text-4xl font-semibold'>Lukas Andersson</h2>
+          <h2 className='text-2xl lg:text-4xl font-semibold mb-6'>Lukas Andersson</h2>
           <p className='text-lg'>
               I am a 19-year-old guy from Stockholm, Sweden. I have just graduated from the Technology program with a focus on information and media technology at Nacka Gymnasium. I have a great interest in technology and programming. I am also interested in music and play several instruments. I am a calm and methodical person who functions well working both in groups, and individually. I am also a creative person who likes to solve problems while also setting goals and structuring the work at task.
-            </p>
+              <br />
+              <br />          
+              On this website you can find my resume, my projects, and my contact information. You can also find my social media accounts and other links.
+          </p>
+          <p className='text-lg'>
+              This website is built using React, NextJS and Tailwind and uses a back-end server built with NodeJS, ExpressJS and MognoDB that handles contact for submissions. The project is open source and is available on my GitHub page.
+          </p>
+          <a href='https://github.com/LukeyBit' className='hover:text-gray-800 flex flex-row gap-2 items-center mt-8'>
+                <FontAwesomeIcon icon={faGithub} className='w-8 h-8' /> /LukeyBit
+            </a>
         </div>
       </div>
     </main>
